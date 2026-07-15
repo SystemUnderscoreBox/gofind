602dcbc6ee8ea3c1418d6be59ea5ad7566e5a9fb
SystemUnderscoreBox <nykanenpaulhenrik@gmail.com>
2026-07-15 20:53:03 +0300

    Add regex support for simple queries like *.mp4

--- gofind.go
+++ gofind.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -11,8 +12,8 @@ import (
 
 // gofind — the soon-to-be replacement for the Unix command "find"
 // Dynamic plan:
-// Add support for regex
-// Add support for flags (e.g. --help)
+// Add support for regex, current scope: support '*' expressions
+// Add support for flags (currently needs support: -d)
 // --help only works if gofind called repo with stdout included, must be worked out on
 // Create test file
 // Compile binary and run
@@ -48,11 +49,42 @@ func main() {
 		findFile = os.Args[1]
 	}
 
+	// In case of regular expressions including one asterisk (*) in input
+	// Call different handle function (handleRegex)
+	// This part is still very WIP !!!
+	// Works for simple queries, e.g. *.pdf
+	if strings.Contains(findFile, "*") && len(findFile) == 1 {
+		fmt.Println("will not search for every file")
+		os.Exit(0)
+	} else if strings.Contains(findFile, "*") {
+		t := time.Now()
+
+		// Call different type of handleInput, i.e. handleRegex
+		if err := handleRegex(findFile); err != nil {
+			fmt.Printf("find failed, error: %v", err)
+			e := time.Now()
+			if os.Args[1] == "-t" || os.Args[1] == "--time-elapsed" {
+				fmt.Println("time elapsed: " + strconv.FormatFloat(e.Sub(t).Seconds(), 'f', -1, 64) + " seconds")
+			}
+			os.Exit(1)
+		}
+
+		e := time.Now()
+		// If flag for time was used, print time elapsed, else skip
+		if os.Args[1] == "-t" || os.Args[1] == "--time-elapsed" {
+			fmt.Println("time elapsed: " + strconv.FormatFloat(e.Sub(t).Seconds(), 'f', -1, 64) + " seconds")
+		}
+
+		os.Exit(0)
+	}
+
 	t := time.Now()
 	if err := handleInput(findFile); err != nil {
 		fmt.Printf("find failed, error: %v", err)
 		e := time.Now()
-		fmt.Println("time elapsed: " + strconv.FormatFloat(e.Sub(t).Seconds(), 'f', -1, 64) + " seconds")
+		if os.Args[1] == "-t" || os.Args[1] == "--time-elapsed" {
+			fmt.Println("time elapsed: " + strconv.FormatFloat(e.Sub(t).Seconds(), 'f', -1, 64) + " seconds")
+		}
 		os.Exit(1)
 	}
 
@@ -72,6 +104,104 @@ func handleHelp() {
 	}
 
 	fmt.Println(string(data))
+}
+
+func handleRegex(input string) error {
+	fmt.Printf("You wrote: %s\n", input)
+
+	seqs := strings.Split(input, "*")
+
+	// TODO: Currently only supports one instance (e.g. "*.mp4")
+	re, err := regexp.Compile(".*(" + seqs[1] + ")")
+
+	if err != nil {
+		return err
+	}
+
+	// Next, start to look for given file.
+	dir, err := os.Getwd()
+
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return errors.New("failed to get current work dir")
+	}
+
+	entries, err := os.ReadDir(dir)
+
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return errors.New("failed to read dir entries")
+	}
+
+	// Call this function recursively.
+	switch err := handleRegexHelper(entries, input, dir, re); err.Error() {
+	// "no match" means complete search has completed, regardless if found or not.
+	case "no match":
+		if len(matches) == 0 {
+			return errors.New("found no matches\n")
+		}
+
+		for match := range len(matches) {
+			coloredMatch := Red + matches[match] + Reset
+			fmt.Printf("found: %s\n", coloredMatch)
+		}
+	// If for some reason a failure happens, return err.
+	case "fail":
+		return err
+	}
+
+	return nil
+}
+
+func handleRegexHelper(entries []os.DirEntry, input string, dir string, re *regexp.Regexp) error {
+	for i := range len(entries) {
+		entry := entries[i]
+
+		if entry.IsDir() {
+			dir = dir + "/" + entry.Name()
+			fmt.Println(dir)
+
+			entriesNew, err := os.ReadDir(dir)
+
+			// Skip to next entry in case ReadDir fails.
+			// Prints out error.
+			if err != nil {
+				fmt.Printf("error: %v\n", err)
+				dir = strings.TrimSuffix(dir, entry.Name())
+				dir = strings.TrimSuffix(dir, "/")
+				continue
+			}
+
+			if len(entriesNew) == 0 {
+				dir = strings.TrimSuffix(dir, entry.Name())
+				dir = strings.TrimSuffix(dir, "/")
+				continue
+			}
+
+			switch err := handleRegexHelper(entriesNew, input, dir, re); err.Error() {
+			// If folder has no matching files, go back and go to next entry.
+			case "no match":
+				dir = strings.TrimSuffix(dir, entry.Name())
+				dir = strings.TrimSuffix(dir, "/")
+				continue
+			// If for some reason a failure happens, return err.
+			case "fail":
+				return err
+			}
+		}
+
+		// Here we check the entry's name, at this point, entry must be a file.
+		str := re.FindString(entry.Name())
+		if str == entry.Name() {
+			dir = dir + "/" + re.FindString(entry.Name())
+			matches = append(matches, dir)
+			dir = strings.TrimSuffix(dir, re.FindString(entry.Name()))
+			dir = strings.TrimSuffix(dir, "/")
+			continue
+		}
+	}
+
+	return errors.New("no match")
 }
 
 func handleInput(input string) error {
