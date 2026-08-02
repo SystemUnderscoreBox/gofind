f81fa3fa5044c77509c288a187c80b1c608fc627
SystemUnderscoreBox <nykanenpaulhenrik@gmail.com>
2026-08-02 15:14:11 +0300

    Further debugging

--- gofind_test.go
+++ gofind_test.go
@@ -14,86 +14,82 @@ func Test_All(t *testing.T) {
 // By default, the tests are run from $HOME, so that proper
 // testing can be carried out.
 func Test_ValidInputs(t *testing.T) {
-
-	dir, _ := os.Getwd()
-	t.Log(os.ReadDir(dir))
-
-	err := exec.Command("gofind", "gofind.go").Run()
+	err := exec.Command("./gofind", "gofind.go").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "--help").Run()
+	err = exec.Command("./gofind", "--help").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-h").Run()
+	err = exec.Command("./gofind", "-h").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-l", "test-file.txt").Run()
+	err = exec.Command("./gofind", "-l", "test-file.txt").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "--literal", "test-file.txt").Run()
+	err = exec.Command("./gofind", "--literal", "test-file.txt").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "*.mp4").Run()
+	err = exec.Command("./gofind", "*.mp4").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-s", os.Getenv("HOME")+"/bin", "gofind").Run()
+	err = exec.Command("./gofind", "-s", os.Getenv("HOME"), "gofind").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "--source", os.Getenv("HOME")+"/bin", "gofind").Run()
+	err = exec.Command("./gofind", "--source", os.Getenv("HOME"), "./gofind").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-s", "-d", "/home", "Downloads").Run()
+	err = exec.Command("./gofind", "-s", "-d", "/home", "Downloads").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-s", "-d", "/home", `"*loads"`).Run()
+	err = exec.Command("./gofind", "-s", "-d", "/home", `"*loads"`).Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "--source", "--directory", "/home", `"*loads"`).Run()
+	err = exec.Command("./gofind", "--source", "--directory", "/home", `"*loads"`).Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-t", "gofind.go").Run()
+	err = exec.Command("./gofind", "-t", "./gofind.go").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "--time-elapsed", "gofind.go").Run()
+	err = exec.Command("./gofind", "--time-elapsed", "./gofind.go").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "-l", "*.mp4").Run()
+	err = exec.Command("./gofind", "-l", "*.mp4").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "--literal", "*.mp4").Run()
+	err = exec.Command("./gofind", "--literal", "*.mp4").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
 
-	err = exec.Command("gofind", "*find*").Run()
+	err = exec.Command("./gofind", "*find*").Run()
 	if err != nil {
 		t.Errorf("TEST FAILED: %v", err)
 	}
